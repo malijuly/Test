c45174c1295b0f18724052d44a8e378a70345810
ML <54767542+malijuly@users.noreply.github.com>
2020-06-28 20:47:24 +0200

    Add files via upload

---
+++ test/src/Test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args){
+
+
+        List<String> lista = new ArrayList<String>();
+
+        lista.add("Magda");
+        System.out.println(lista.get(1));
+
+    }
+
+
+}
